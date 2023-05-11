c0e79177af1d8706d7028b902a0eec396ea1e3e4
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2023-05-11 07:50:02 -0400

    test requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 from rec import generate_playlist
-import streamlit as st
+import requests
 
 
 def dict_to_string(d):
@@ -28,6 +26,33 @@ def dict_to_string(d):
 music_request = st.text_area(
     "\"I'm DJing a really cool party in Manhattan. I need music that won't interrupt the conversation but will get people moving and bobbing their heads.\""
 )
+
+
+AUTH_URL = "https://accounts.spotify.com/api/token"
+
+# POST
+auth_response = requests.post(
+    AUTH_URL,
+    {
+        "grant_type": "client_credentials",
+        "client_id": st.secrets["SPOTIPY_CLIENT_ID"],
+        "client_secret": st.secrets["SPOTIPY_CLIENT_SECRET"],
+    },
+)
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data["access_token"]
+
+
+url = "https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V"
+headers = {"Authorization": "Bearer {token}".format(token=access_token)}
+
+
+print(requests.get(url=url, headers=headers).json())
+
 
 if music_request:
     if len(music_request) > 2:
