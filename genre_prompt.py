20dc6cb7c619bb7b5f0b379ff28852a80ffd06c7
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2023-05-13 09:30:00 -0400

    beignning to implement enhanced playlists

--- genre_prompt.py
+++ genre_prompt.py
@@ -3,7 +3,7 @@ from available_genres import list_of_recommendation_genres
 
 beginning_of_json = """{"genres":"""
 
-audio_features = """Formatted as: variable_name (range): description
+audio_features = """Formatted as: variable_name (range): description. For each of the attributes below (except mode), provide a min and max. 
 ```
 1. acousticness (0-1): Confidence in the track being acoustic. Higher values indicate higher confidence.
 2. danceability (0-1): Suitability for dancing based on tempo, rhythm stability, beat strength, and regularity.
@@ -14,18 +14,18 @@ audio_features = """Formatted as: variable_name (range): description
 7. loudness (float): Overall loudness averaged across the track, typically between -60 and 0 dB.
 8. mode (0 or 1): Modality of the track. 1 for major, 0 for minor.
 9. speechiness (0-1): Presence of spoken words. Higher values indicate more speech-like content.
-10. tempo (dict(min=float, max=float)): Estimated tempo in beats per minute.
+10. tempo (int): Estimated tempo in beats per minute.
 11. valence (0-1): Musical positiveness. Higher values indicate more positive-sounding tracks.
 12. popularity (0-100): Integer representing popularity of a track is a value between 0 and 100, with 100 being the most popular.
 ```"""
 
-prompt = """This is the list of genres available on spotify: {list_of_recommendation_genres}.
+prompt = """Here are the audio features available in the spotify api: {audio_features}
 
-Additionally, here are the audio features available in the spotify api: {audio_features}
+When asked about what I'm in the mood to listen to, I replied: `{{music_request}}`. 
 
-When asked about what I'm in the mood for, I replied "{{music_request}}". 
+Using this information, I want you to make a JSON that contains appropriate values for the attributes listed. Not every attribute needs to be listed, just the ones that you think are important to specify. This JSON will be passed to spotify to make a playlist. Be sure to include a fun, six word summary of my mood as the name of the playlist. The final JSON should look like `{{beginning_of_json}} [...], "playlist_name": ..., "energy": {{{{"min": ..., "max": ...}}}}, ...`
 
-Using this information, I want you to make a JSON that contains appropriate values for the attributes listed. Not every attribute needs to be listed, just the ones that you think are important to specify. This JSON will be passed to spotify to make a playlist. Be sure to include an up-to-ten-word summary of my mood as the name of the playlist. The final JSON should look like `{{beginning_of_json}} [...], "playlist_name": ..., "energy": ...`, ...
+This is the list of genres available on spotify: {list_of_recommendation_genres}. Don't recommend genres outside of that list. Finally, feel free to suggest up to 4 artists as a list of names under the key "artists".
 
 {{beginning_of_json}}""".format(
     audio_features=audio_features,
--- streamlit_app.py
+++ streamlit_app.py
@@ -191,9 +191,26 @@ def app_display_welcome():
         st.markdown(link_html, unsafe_allow_html=True)
 
 
+def range_min_max_to_target(min_max_dict):
+    """Takes in a dictionary with min,max keys. Returns a dictionary with target keys where target=mean(min, max)"""
+    return {"target": (min_max_dict["min"] + min_max_dict["max"]) / 2}
+
+
+def convert_min_max_parameters_to_target(query_dict):
+    """Assumes it gets {other:val, ..., min_key:, max_key:, min_key1:, max_key1:, ...}"""
+    new_query_dict = {}
+    for key, val in query_dict.items():
+        if isinstance(val, dict):
+            new_query_dict[key] = range_min_max_to_target(val)
+        else:
+            new_query_dict[key] = val
+    return new_query_dict
+
+
 def get_recommendation_parameters(sp, prompt, debug=True):
     if debug:
         generated_json = """{"genres":["trap", "dancehall", "rap", "hip-hop"],
+ "artists": ["Shakewell", "Yung Gravy"],
  "energy": 0.68,
  "danceability": 0.8,
  "tempo": 150,
@@ -204,7 +221,7 @@ def get_recommendation_parameters(sp, prompt, debug=True):
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
-            max_tokens=150,
+            max_tokens=300,
             n=1,
             stop=None,
             temperature=1.0,
@@ -219,10 +236,9 @@ def get_recommendation_parameters(sp, prompt, debug=True):
         print(playlist_data)
 
         if "artists" in playlist_data:
-            playlist_data["seed_artists"] = get_artist_ids(
-                sp, playlist_data.pop("artists")
-            )
-            playlist_data.pop("seed_genres")
+            playlist_data["seed_artists"] = get_artist_ids(sp, playlist_data["artists"])
+            if len(playlist_data["seed_artists"]) > 1:
+                playlist_data["seed_genres"] = playlist_data["seed_genres"][:1]
 
     except json.decoder.JSONDecodeError:
         print(generated_json)
@@ -236,12 +252,11 @@ def get_recommendation_parameters(sp, prompt, debug=True):
 def fix_dict_keys(spotify_search_dict: dict):
     new_search_dict = {}
     for k, v in spotify_search_dict.items():
-        new_k = k
-        if "artist" not in k and "genre" not in k:
-            if isinstance(v, float) or isinstance(v, int):
-                new_k = f"target_{k}"
-
-        new_search_dict[new_k] = spotify_search_dict[k]
+        if isinstance(v, dict):
+            for range_key, range_val in v.items():
+                new_search_dict[f"{range_key}_{k}"] = range_val
+        else:
+            new_search_dict[k] = spotify_search_dict[k]
     new_search_dict["seed_genres"] = filter_genres(spotify_search_dict["genres"])
     return new_search_dict
 
@@ -255,6 +270,11 @@ def filter_genres(genre_names):
         print(genre_names)
         raise ValueError("No genres returned!")
     return remaining
+
+
+def find_artist(sp, artist_id):
+    uri = f"spotify:artist:{artist_id}"
+    return sp.artist(uri)
 
 
 def get_artist_ids(sp, artist_names):
@@ -271,7 +291,7 @@ def get_artist_ids(sp, artist_names):
     return artists
 
 
-def get_track_recommendations(sp, **kwargs):
+def get_track_recommendations(sp, genres=[""], **kwargs):
     tracks = sp.recommendations(**kwargs)["tracks"]
     track_names = [
         f'{track["name"]} - {track["artists"][0]["name"]}' for track in tracks
@@ -285,6 +305,10 @@ def add_tracks_to_queue(sp, track_ids):
     for track_id in track_ids:
         sp.add_to_queue(f"spotify:track:{track_id}")
     print("Tracks added to the queue successfully!")
+
+
+def get_track_uris(track_ids):
+    return [f"spotify:track:{track_id}" for track_id in track_ids]
 
 
 def create_spotify_playlist(sp, track_ids, playlist_name="Test", music_request=""):
@@ -304,21 +328,51 @@ def create_spotify_playlist(sp, track_ids, playlist_name="Test", music_request="
     playlist_id = playlist["id"]
 
     # Add tracks to the playlist
-    track_uris = [f"spotify:track:{track_id}" for track_id in track_ids]
+    track_uris = get_track_uris(track_ids)
+    print(track_uris)
     sp.playlist_add_items(playlist_id, track_uris)
     print(f"Playlist '{playlist_name}' created and tracks added successfully!")
     return playlist_id
 
 
-def generate_playlist(music_request=None, debug=False):
+def get_seed_tracks_query(playlist_data, track_ids, n=5):
+    """This function takes in a list of tracks, chooses n(<=5) random ones, then uses those to get recs from spotify"""
+    random_tracks = random.sample(track_ids, min(n, len(track_ids)))
+
+    # clear out old search parameters so we don't get an error in our query
+    for key in ["seed_genres", "seed_artists", "seed_tracks"]:
+        if key in playlist_data:
+            playlist_data.pop(key)
+
+    playlist_data["seed_tracks"] = [track for track in random_tracks]
+    return playlist_data
+
+
+def generate_playlist(music_request=None, debug=False, num_songs=20):
     music_request = music_request or ""
 
     formatted_prompt = prompt.format(
         beginning_of_json=beginning_of_json, music_request=music_request
     )
-    playlist_data = get_recommendation_parameters(sp, formatted_prompt, debug=debug)
+    raw_playlist_query = get_recommendation_parameters(
+        sp, formatted_prompt, debug=debug
+    )
     print("Got playlist parameters")
-    track_ids = get_track_recommendations(sp, **playlist_data)
+    range_playlist_query = fix_dict_keys(raw_playlist_query)
+    track_ids = get_track_recommendations(sp, **range_playlist_query)
+    print(len(track_ids))
+    while len(track_ids) < num_songs:
+        print("Enhancing the playlist...")
+        # The previous search was too narrow so we relax the constraints
+        playlist_data = convert_min_max_parameters_to_target(range_playlist_query)
+        print(playlist_data)
+        if track_ids:
+            playlist_data = get_seed_tracks_query(playlist_data, track_ids)
+        print(playlist_data)
+
+        new_track_ids = get_track_recommendations(sp, **playlist_data)
+        track_ids.extend(new_track_ids[: num_songs - len(track_ids)])
+        print(len(track_ids))
     print("Got track ids")
 
     if "playlist_name" in playlist_data:
@@ -329,7 +383,7 @@ def generate_playlist(music_request=None, debug=False):
     playlist_id = create_spotify_playlist(
         sp, track_ids, playlist_name=playlist_name, music_request=music_request
     )
-    print("Made the playlistMade the playlist")
+    print("Made the playlist")
     # sp.start_playback(playlist_id)
     # add_tracks_to_queue(sp, track_ids)
     return playlist_data, playlist_name
@@ -466,7 +520,7 @@ if st.session_state["signed_in"]:
     name = user["display_name"]
     SPOTIPY_USERNAME = user["id"]
 
-    music_request = st.text_area('"I want to listen to Van Gogh’s starry night ."')
+    music_request = st.text_area("", "I want to listen to Van Gogh’s starry night")
 
     if music_request and music_request != st.session_state["music_request"]:
         if len(music_request) > 2:
