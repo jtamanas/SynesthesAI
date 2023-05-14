a71df3199d807804826f1a8796b4aa8973147e28
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2023-05-13 22:12:45 -0400

    finish refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,402 +1,110 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  1 19:35:20 2022
-
-@author: jtamanas 
-
-but huge shoutout to @chorg for figuring out how to log into spotify https://github.com/chorgan182/PlaylistPreserver/blob/qry-param-method/playlist_preserver_app.py
-"""
-# libraries
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotify_handler import SpotifyHandler
+from playlist_generator import PlaylistGenerator
 import streamlit as st
-import os
-import random
-from genre_prompt import beginning_of_json, prompt
-import json
-import openai
-from available_genres import recommendation_genres
 from css import css
 from utils import dict_to_string
 
 
-def get_token(oauth, code):
-    token = oauth.get_access_token(code, as_dict=False, check_cache=False)
-    # remove cached token saved in directory
-    os.remove(".cache")
-    # return the token
-    return token
+class App:
+    def __init__(
+        self,
+        spotify_handler,
+    ):
+        self.spotify_handler = spotify_handler
+        self.playlist_generator = PlaylistGenerator(spotify_handler=spotify_handler)
 
-
-def sign_in(token):
-    sp = spotipy.Spotify(auth=token)
-    return sp
-
-
-def app_get_token():
-    try:
-        token = get_token(st.session_state["oauth"], st.session_state["code"])
-    except Exception as e:
-        st.error("An error occurred during token retrieval!")
-        st.write("The error is as follows:")
-        st.write(e)
-    else:
-        st.session_state["cached_token"] = token
-
-
-def app_sign_in():
-    try:
-        sp = sign_in(st.session_state["cached_token"])
-    except Exception as e:
-        st.error("An error occurred during sign-in!")
-        st.write("The error is as follows:")
-        st.write(e)
-    else:
-        st.session_state["signed_in"] = True
-        app_display_welcome()
-
-    return sp
-
-
-def range_min_max_to_target(min_max_dict):
-    """Takes in a dictionary with min,max keys. Returns a dictionary with target keys where target=mean(min, max)"""
-    return {"target": (min_max_dict["min"] + min_max_dict["max"]) / 2}
-
-
-def get_playlist_query_with_targets(query_dict):
-    """Assumes it gets {other:val, ..., min_key:, max_key:, min_key1:, max_key1:, ...}"""
-    new_query_dict = {}
-    for key, val in query_dict.items():
-        if isinstance(val, dict):
-            new_query_dict[key] = range_min_max_to_target(val)
-        else:
-            new_query_dict[key] = val
-    return new_query_dict
-
-
-def get_recommendation_parameters(sp, prompt, debug=True):
-    if debug:
-        generated_json = """{"genres":["classical", "ambient"],
-"playlist_name": "[TEST] Starry Night Vibes",
-"energy": {"min": 0, "max": 0.3},
-"danceability": {"min": 0, "max": 0.3},
-"valence": {"min": 0.6, "max": 0.9},
-"acousticness": {"min": 0.7, "max": 1},
-"artists": ["Ludovico Einaudi", "Nujabes", "Gustavo Santaolalla", "Hans Zimmer"]
-}"""
-    else:
-        # Send the prompt to the OpenAI API
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=300,
-            n=1,
-            stop=None,
-            temperature=1.0,
+    def title(self):
+        st.markdown(
+            "# Synesthes<span style='color:#ff6319'>ai</span>", unsafe_allow_html=True
         )
-        # Extract the generated JSON from the response
-        generated_json = beginning_of_json + response.choices[0].text.strip()
-        print(generated_json)
-    try:
-        playlist_data = json.loads(generated_json)
 
-        playlist_data["seed_genres"] = filter_genres(playlist_data["genres"])
+    def display_welcome(self):
+        # define welcome
+        welcome_msg = """
+        Write a really specific description of your mood. I'll pass that onto Spotify
+        to build a playlist just for you.
+        """
 
-        if "artists" in playlist_data:
-            playlist_data["seed_artists"] = get_ids_from_search(
-                sp, playlist_data["artists"], search_type="artist"
-            )
-            if len(playlist_data["seed_artists"]) > 1:
-                playlist_data["seed_genres"] = playlist_data["seed_genres"][:1]
+        # define temporary note
+        note_temp = """
+        Example: 
+        
+        _"I'm DJing a really cool party in Manhattan. I need music that won't interrupt the conversation but will get people moving and bobbing their heads. I'll be laughed out of the building if I play anything on the Billboard top 100 so keep it underground."_
+        """
+        auth_url = self.spotify_handler.oauth.get_authorize_url()
 
-        if "tracks" in playlist_data:
-            playlist_data["seed_tracks"] = get_ids_from_search(
-                sp, playlist_data["tracks"], search_type="tracks"
-            )
-            if len(playlist_data["seed_tracks"]) > 1:
-                playlist_data["seed_artists"] = playlist_data["seed_artists"][:1]
+        link_html = '<a target="_blank" href="{url}" >{msg}</a>'.format(
+            url=auth_url,
+            msg="Sign into Spotify.",
+            unsafe_allow_html=True,
+        )
 
-    except json.decoder.JSONDecodeError:
-        print(generated_json)
-        print("FAILED TO GENERATE VALID JSON")
+        self.title()
 
-    print()
+        if not st.session_state["signed_in"]:
+            st.markdown(welcome_msg)
+            st.markdown(note_temp)
+            st.markdown(link_html, unsafe_allow_html=True)
 
-    return playlist_data
+    def run(self):
+        # setup the page
+        # Add some styling with CSS selectors
+        st.markdown(css, unsafe_allow_html=True)
 
+        # app session variable initialization
+        if "signed_in" not in st.session_state:
+            st.session_state["signed_in"] = False
+        if "cached_token" not in st.session_state:
+            st.session_state["cached_token"] = ""
+        if "code" not in st.session_state:
+            st.session_state["code"] = ""
 
-def get_playlist_query_with_ranges(spotify_search_dict: dict):
-    new_search_dict = {}
-    for k, v in spotify_search_dict.items():
-        if isinstance(v, dict):
-            for range_key, range_val in v.items():
-                new_search_dict[f"{range_key}_{k}"] = range_val
-        else:
-            new_search_dict[k] = spotify_search_dict[k]
-    return new_search_dict
+        # set scope and establish connection
+        scopes = " ".join(
+            [
+                "playlist-read-private",
+                "playlist-modify-public",
+            ]
+        )
+        # authenticate with response stored in url
 
+        if not self.spotify_handler.app_authenticated():
+            self.display_welcome()
 
-def filter_genres(genre_names):
-    remaining = []
-    for genre in genre_names:
-        if genre in recommendation_genres:
-            remaining.append(genre)
-    if not remaining:
-        print(genre_names)
-        raise ValueError("No genres returned!")
-    return remaining
+        # after auth, get user info
+        # only display the following after login
+        ### is there another way to do this? clunky to have everything in an if:
 
+        if "music_request" not in st.session_state:
+            st.session_state[
+                "music_request"
+            ] = "i want to listen to van goghs starry night 👨‍🎨🌃🌌🖼️ "
 
-def find_artist(sp, artist_id):
-    uri = f"spotify:artist:{artist_id}"
-    return sp.artist(uri)
+        if st.session_state["signed_in"]:
+            self.title()
+            # sp = st.session_state["sp"]
+            user = self.spotify_handler.spotify.current_user()
+            # name = user["display_name"]
+            username = user["id"]
 
+            music_request = st.text_area("", st.session_state["music_request"])
 
-def get_ids_from_search(sp, names, search_type="artist"):
-    """search_type can be artist or tracks currently"""
-    print("getting artists ids")
-    pieces = []
-    for name in names:
-        print(name)
-        # there's some weirdness with the search. The limit=1
-        # result is not the top result for some reason.
-        # get top 10 and then only look at top 1
-        result = sp.search(name, type=search_type, limit=10)
-        pieces.append(result[f"{search_type}s"]["items"][0]["id"])
+            if music_request and music_request != st.session_state["music_request"]:
+                if len(music_request) > 2:
+                    st.text("Processing your request...")
+                    (
+                        playlist_data,
+                        playlist_name,
+                    ) = self.playlist_generator.generate_playlist(
+                        username=username, music_request=music_request, debug=False
+                    )
 
-    return pieces
-
-
-def get_track_recommendations(sp, genres=[""], **kwargs):
-    # im not sure if list(set(tracks)) removes duplicates so I'll only do it on track ids
-    tracks = sp.recommendations(**kwargs)["tracks"]
-    track_names = [
-        f'{track["name"]} - {track["artists"][0]["name"]}' for track in tracks
-    ]
-    track_ids = [track["id"] for track in tracks]
-
-    # remove duplicates
-    track_ids = list(set(track_ids))
-    print("\n".join(track_names))
-    return track_ids
-
-
-def add_tracks_to_queue(sp, track_ids):
-    for track_id in track_ids:
-        sp.add_to_queue(f"spotify:track:{track_id}")
-    print("Tracks added to the queue successfully!")
+                    st.session_state["music_request"] = music_request
+                    st.text(dict_to_string(playlist_data))
+                    # st.markdown("_Picture to Playlist coming soon..._")
 
 
-def get_track_uris(track_ids):
-    return [f"spotify:track:{track_id}" for track_id in track_ids]
-
-
-def create_spotify_playlist(sp, track_ids, playlist_name="Test", music_request=""):
-    # Create a new playlist
-    playlist = sp.user_playlist_create(
-        SPOTIPY_USERNAME,
-        playlist_name,
-        public=True,
-        description=music_request,
-    )
-    playlist_url = playlist["external_urls"]["spotify"]
-    playlist_url = '<a target="_blank" href="{url}" >Now on Spotify: {msg}</a>'.format(
-        url=playlist_url, msg=playlist_name
-    )
-
-    st.markdown(playlist_url, unsafe_allow_html=True)
-    playlist_id = playlist["id"]
-
-    # Add tracks to the playlist
-    track_uris = get_track_uris(track_ids)
-    print(track_uris)
-    sp.playlist_add_items(playlist_id, track_uris)
-    print(f"Playlist '{playlist_name}' created and tracks added successfully!")
-    return playlist_id
-
-
-def get_seed_tracks_query(playlist_data, track_ids, n=5):
-    """This function takes in a list of tracks, chooses n(<=5) random ones, then uses those to get recs from spotify"""
-    random_tracks = random.sample(track_ids, min(n, len(track_ids)))
-
-    # clear out old search parameters so we don't get an error in our query
-    for key in ["seed_genres", "seed_artists", "seed_tracks"]:
-        if key in playlist_data:
-            playlist_data.pop(key)
-
-    playlist_data["seed_tracks"] = [track for track in random_tracks]
-    return playlist_data
-
-
-def generate_playlist(
-    music_request=None, debug=False, num_tracks=20, num_enhanced_tracks_to_add=3
-):
-    """
-    num_enhanced_tracks_to_add: is small because searching by target is often not very precise.
-    But if we take the top k searches from a targetted search and then use those, it's possible
-    that the resulting recommendations are more similar to the targets, than if we were to
-    just search by target alone.
-        This is because there are hidden attributes that are not available as targets for us,
-        but that could be correlated amongst the top k.
-
-        Not a good explanation, but it makes sense I swear.
-    """
-    music_request = music_request or ""
-
-    formatted_prompt = prompt.format(
-        beginning_of_json=beginning_of_json, music_request=music_request
-    )
-    raw_playlist_query = get_recommendation_parameters(
-        sp, formatted_prompt, debug=debug
-    )
-    print("Got playlist parameters")
-    range_playlist_query = get_playlist_query_with_ranges(raw_playlist_query)
-    track_ids = get_track_recommendations(sp, **range_playlist_query)
-    print(len(track_ids))
-    while len(track_ids) < num_tracks:
-        print("Enhancing the playlist...")
-        # The previous search was too narrow so we relax the constraints
-        target_range_query = get_playlist_query_with_targets(raw_playlist_query)
-        print(target_range_query)
-        if track_ids:
-            target_range_query = get_seed_tracks_query(target_range_query, track_ids)
-        print(target_range_query)
-
-        new_track_ids = get_track_recommendations(sp, **target_range_query)
-        track_ids.extend(new_track_ids[:num_enhanced_tracks_to_add])
-
-        # remove duplicate track ids
-        track_ids = list(set(track_ids))
-        print(len(track_ids))
-    print("Got track ids")
-
-    if "playlist_name" in raw_playlist_query:
-        playlist_name = raw_playlist_query["playlist_name"]
-    else:
-        playlist_name = "For your mood"
-
-    playlist_id = create_spotify_playlist(
-        sp, track_ids, playlist_name=playlist_name, music_request=music_request
-    )
-    print("Made the playlist")
-    st.balloons()
-
-    # sp.start_playback(playlist_id)
-    # add_tracks_to_queue(sp, track_ids)
-    return raw_playlist_query, playlist_name
-
-
-def app_display_welcome():
-    # define welcome
-    welcome_msg = """
-    Write a really specific description of your mood. I'll pass that onto Spotify
-    to build a playlist just for you.
-    """
-
-    # define temporary note
-    note_temp = """
-    Example: 
-    
-    _"I'm DJing a really cool party in Manhattan. I need music that won't interrupt the conversation but will get people moving and bobbing their heads. I'll be laughed out of the building if I play anything on the Billboard top 100 so keep it underground."_
-    """
-
-    st.markdown(
-        "# Synesthes<span style='color:#ff6319'>ai</span>", unsafe_allow_html=True
-    )
-
-    if not st.session_state["signed_in"]:
-        st.markdown(welcome_msg)
-        st.markdown(note_temp)
-        st.markdown(link_html, unsafe_allow_html=True)
-
-
-# setup the page
-# Add some styling with CSS selectors
-st.markdown(css, unsafe_allow_html=True)
-
-# app session variable initialization
-if "signed_in" not in st.session_state:
-    st.session_state["signed_in"] = False
-if "cached_token" not in st.session_state:
-    st.session_state["cached_token"] = ""
-if "code" not in st.session_state:
-    st.session_state["code"] = ""
-
-# set scope and establish connection
-scopes = " ".join(
-    [
-        "playlist-read-private",
-        "playlist-modify-public",
-    ]
-)
-
-# create oauth object
-oauth = SpotifyOAuth(
-    scope=scopes,
-    redirect_uri=st.secrets["SPOTIPY_REDIRECT_URI"],
-    client_id=st.secrets["SPOTIPY_CLIENT_ID"],
-    client_secret=st.secrets["SPOTIPY_CLIENT_SECRET"],
-)
-# store oauth in session
-st.session_state["oauth"] = oauth
-
-# retrieve auth url
-auth_url = oauth.get_authorize_url()
-
-link_html = '<a target="_blank" href="{url}" >{msg}</a>'.format(
-    url=auth_url,
-    msg="Sign into Spotify.",
-    unsafe_allow_html=True,
-)
-
-if "oauth" not in st.session_state:
-    # st.session_state["oauth"] = None
-    st.session_state["oauth"] = oauth
-    print("WE RESET THE OAUTH SESSION STATE")
-
-# authenticate with response stored in url
-
-# get current url (stored as dict)
-url_params = st.experimental_get_query_params()
-
-# attempt sign in with cached token
-if st.session_state["cached_token"] != "":
-    sp = app_sign_in()
-# if no token, but code in url, get code, parse token, and sign in
-elif "code" in url_params and st.session_state["oauth"] is not None:
-    # all params stored as lists, see doc for explanation
-    st.session_state["code"] = url_params["code"][0]
-    app_get_token()
-    sp = app_sign_in()
-    st.session_state["sp"] = sp
-# otherwise, prompt for redirect
-else:
-    app_display_welcome()
-
-# after auth, get user info
-# only display the following after login
-### is there another way to do this? clunky to have everything in an if:
-
-if "music_request" not in st.session_state:
-    st.session_state[
-        "music_request"
-    ] = "i want to listen to van goghs starry night 👨‍🎨🌃🌌🖼️ "
-
-if st.session_state["signed_in"]:
-    sp = st.session_state["sp"]
-    user = sp.current_user()
-    name = user["display_name"]
-    SPOTIPY_USERNAME = user["id"]
-
-    music_request = st.text_area("", st.session_state["music_request"])
-
-    if music_request and music_request != st.session_state["music_request"]:
-        if len(music_request) > 2:
-            st.text("Processing your request...")
-            playlist_data, playlist_name = generate_playlist(
-                music_request=music_request, debug=False
-            )
-
-            st.session_state["music_request"] = music_request
-            st.text(dict_to_string(playlist_data))
-            # st.markdown("_Picture to Playlist coming soon..._")
+if __name__ == "__main__":
+    spotify_handler = SpotifyHandler()
+    app = App(spotify_handler)
+    app.run()
