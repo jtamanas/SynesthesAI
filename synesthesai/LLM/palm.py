bd700ba504e06109abbc52459c6088dfadce0448
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2023-05-23 22:20:06 -0400

    remove safety filter on palm

--- synesthesai/LLM/palm.py
+++ synesthesai/LLM/palm.py
@@ -1,16 +1,36 @@
-from LLM.base import BaseLLM
+from base import BaseLLM
 import google.generativeai as palm
+from google.generativeai.types import safety_types
 import streamlit as st
+import os
 
 class PaLM(BaseLLM):
     def __init__(self, model: str = "models/text-bison-001") -> None:
-        palm.configure(api_key=st.secrets["PALM_API_KEY"])
+        palm.configure(api_key=os.environ["PALM_API_KEY"])
+        # palm.configure(api_key=st.secrets["PALM_API_KEY"])
         models = [
             m for m in palm.list_models() 
             if ('generateText' in m.supported_generation_methods) 
             and (m.name == model)
         ]
         model = models[0].name
+        
+        harm_categories = [
+            "DEROGATORY",
+            "TOXICITY",
+            "SEXUAL",
+            "VIOLENCE", 
+            "DANGEROUS",
+            "MEDICAL", 
+        ]
+        self.safety_settings = [
+            {
+                "category": getattr(safety_types.HarmCategory, f"HARM_CATEGORY_{category}"),
+                "threshold": safety_types.HarmBlockThreshold.BLOCK_NONE,
+            }
+            for category in harm_categories
+        ]
+        
         super().__init__(model=model)
         
     def complete(self, prompt: str, temperature: float=1.0, max_tokens: int=300) -> str:
@@ -22,6 +42,7 @@ class PaLM(BaseLLM):
             prompt=prompt,
             temperature=temperature,
             max_output_tokens=max_tokens,
+            safety_settings=self.safety_settings
         )
         
         return response.result
